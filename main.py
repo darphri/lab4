3f232b14c941570222bd6b44b77a3762654d8e86
dariiapoloz <dariiapoloz@knu.ua>
2026-03-01 20:19:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import math
-from utils import factorial, is_prime, fibonacci
+from utils import factorial, is_prime, fibonacci, is_even, is_palindrome
 n=int(input())
+word=input()
 print(factorial(n))
 print(is_prime(n))
 print(fibonacci(n))
+print(is_even(n))
+print(is_palindrome(word))  
